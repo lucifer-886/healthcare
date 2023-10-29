915b5e7767dd786522327b2aa1a9da2e6a5b203d
lucifer-886 <chethanb886@gmail.com>
2023-10-29 08:59:48 +0530

    done

---
+++ src/Scripts/reviews.js
@@ -0,0 +1,22 @@
+export const customerReviews = [
+    {
+      "name": "ashok",
+      "location": "Bangalore,India",
+      "message": "Health Plus transformed my healthcare experience! The online consultations were so convenient, and the doctors were knowledgeable and caring."
+    },
+    {
+      "name": "Likit",
+      "location": "Bangalore,India",
+      "message": "I found the perfect specialist for my condition through Health Plus. The personalized treatment plan made all the difference. Thank you for prioritizing my health!"
+    },
+    {
+      "name": "Joshua",
+      "location": "Mangalore,India",
+      "message": "Booking appointments was a breeze, and the service exceeded my expectations. The doctors truly listen and provide effective solutions. 5 stars!"
+    },
+    {
+      "name": "Harshith",
+      "location": "B C Road,India",
+      "message": "Health Plus is a game-changer! The emergency care saved me during a critical situation. Grateful for their quick and efficient support."
+    },
+];
